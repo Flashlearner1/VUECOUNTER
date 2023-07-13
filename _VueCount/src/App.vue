f0634e558b184ed0539bea9db778bdfeaa1c7407
Flashlearner1 <osayandekingsley768@gmail.com>
2023-07-13 08:14:05 -0700

    _VueCount

---
+++ _VueCount/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+</script>
+
+<template>
+  <h4>Current count is : </h4>
+  <h1>{{ count }}</h1>
+  <button @click="count++">+</button>
+  <button @click="count--">-</button>
+</template>
+
+//"count" is a javascript//
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
